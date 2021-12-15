6a53eb65b5c51339f6638b3dba87d0551d99503d
Oscar España <espaaoscar@gmail.com>
2021-12-15 16:33:52 -0500

    Create the Hero section

---
+++ components/Hero/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="bg-black text-white">
+      <div className="container pt-40 pb-56 relative">
+        <h1 className="flex flex-col uppercase">
+          <span className="text-[28px] leading-[1.15] md:text-[38px] lg:text-5xl font-medium">
+            Hola soy
+          </span>
+          <span className="text-primary text-4xl leading-[1.15] md:text-[54px] lg:text-7xl font-bold lg:py-2">
+            Oscar España
+          </span>
+          <span className="text-4xl leading-[1.15] md:text-[54px] lg:text-7xl font-bold">
+            Desarrollador Web
+          </span>
+        </h1>
+        <button className="mt-14 px-12 py-3 text-xl font-medium text-primary border border-primary rounded-lg cursor-pointer">
+          Contáctame
+        </button>
+        <p
+          className="text-[40px] md:text-[54px] lg:text-7xl font-bold text-gray uppercase opacity-[15%] 
+          writing-mode-vertical absolute bottom-12 lg:bottom-10 right-8 lg:right-[120px]"
+        >
+          {"<Developer/>"}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
