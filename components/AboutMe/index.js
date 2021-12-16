2a524d62f7c746e89e72ea6434fdc1edf9b15e2f
Oscar España <espaaoscar@gmail.com>
2021-12-16 13:33:36 -0500

    Create the AboutMe section: technologies

--- components/AboutMe/index.js
+++ components/AboutMe/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import { skills } from '../../constants/skill'
+import Skill from "./Skill";
 
 const AboutMe = () => {
   return (
-    <section className="bg-gray">
-      <div className="container grid grid-cols-12 py-16 md:py-28 lg:py-40">
+    <section className="bg-gray py-16 md:py-28 lg:py-40">
+      <div className="container grid grid-cols-12 ">
         <div className="col-span-12 md:col-span-5 flex flex-col justify-center">
           <h2>
             <span className="text-primary">Acerca</span> de mi
@@ -37,7 +39,21 @@ const AboutMe = () => {
           />
         </div>
       </div>
-    </section>
+      <div className="container mt-6 md:mt-14 lg:mt-24 ">
+        <div className='rounded-3xl overflow-hidden'>
+          <div className='bg-black custom-background p-8 sm:p-12 lg:p-16'>
+            <h3 className='text-white text-2xl font-bold mb-8'>Tecnologías</h3>
+            <div className='columns-3 sm:columns-2 md:columns-3 lg:columns-4 space-y-5'>
+              {skills.map((skill, index) => (
+                <Skill key={index} skill={skill.title} image={skill.image} />
+              ))}
+            </div>
+          </div>
+          <div className="bg-primary w-full h-[10px]" />
+        </div >
+      </div>
+
+    </section >
   );
 };
 
---
+++ constants/skill.js
@@ -0,0 +1,72 @@
+import html from "../public/img/logos/html.svg"
+import css from "../public/img/logos/css3.svg"
+import js from "../public/img/logos/js.svg"
+import git from "../public/img/logos/git.svg"
+import react from "../public/img/logos/react.svg"
+import tailwind from "../public/img/logos/tailwind-css.svg"
+import bootstrap from "../public/img/logos/bootstrap.svg"
+import next from "../public/img/logos/next-js.svg"
+import node from "../public/img/logos/node.svg"
+import mongodb from "../public/img/logos/mongodb.svg"
+import wordpress from "../public/img/logos/wordpress.svg"
+import figma from "../public/img/logos/figma.svg"
+import docker from "../public/img/logos/docker.svg"
+
+export const skills = [
+  {
+    image: html,
+    title: "HTML"
+  },
+  {
+    image: css,
+    title: "CSS"
+  },
+  {
+    image: js,
+    title: "JS"
+  },
+  {
+    image: git,
+    title: "Git"
+  },
+  {
+    image: react,
+    title: "React"
+  },
+  {
+    image: tailwind,
+    title: "Tailwind CSS"
+  },
+  {
+    image: bootstrap,
+    title: "Bootstrap"
+  },
+  {
+    image: next,
+    title: "Next JS"
+  },
+  {
+    image: node,
+    title: "Node JS"
+  },
+  {
+    image: react,
+    title: "React Native"
+  },
+  {
+    image: mongodb,
+    title: "Mongo DB"
+  },
+  {
+    image: wordpress,
+    title: "Wordpress"
+  },
+  {
+    image: figma,
+    title: "Figma"
+  },
+  {
+    image: docker,
+    title: "Docker"
+  },
+]
