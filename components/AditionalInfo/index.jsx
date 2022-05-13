2a3cda54c71159e6cecbc98bd920c4bc29681567
Oscar España <espaaoscar@gmail.com>
2022-05-13 00:09:57 -0500

    feat: add animation in AditionInfo section, update styes in Skills section

--- components/AditionalInfo/index.jsx
+++ components/AditionalInfo/index.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Card from "./Card";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const AditionalInfo = () => {
+  useEffect(() => {
+    const services = gsap.utils.toArray("#services div");
+
+    ScrollTrigger.matchMedia({
+      "(max-width: 767px)": function () {
+        services.forEach((service) => {
+          gsap.timeline({
+            scrollTrigger: {
+              trigger: service,
+              start: "top center",
+              end: "bottom center",
+              scrub: true,
+              toggleClass: "border-card-service",
+            },
+          });
+        });
+      },
+    });
+  }, []);
+
   return (
     <section className="bg-black pt-20 md:pt-24 pb-36 text-white">
       <div className="container">
@@ -12,7 +36,10 @@ const AditionalInfo = () => {
           enfoco en entregar un producto de calidad de acuerdo a las necesidades
           del usuario
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-36">
+        <div
+          id="services"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-36"
+        >
           <Card
             urlImage="/iconos/desktop.svg"
             description="Desarrollo de aplicaciones web, sitios web, E -commerce"
--- components/Skills/index.jsx
+++ components/Skills/index.jsx
@@ -5,19 +5,9 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Skills = () => {
-  const skillsSectionRef = useRef(null);
-  const skillsRef = useRef(null);
-
   return (
-    <section
-      ref={skillsSectionRef}
-      id="skills"
-      className="animated-gradient overflow-hidden"
-    >
-      <div
-        ref={skillsRef}
-        className=" py-12 md:py-16 relative flex min-w-full w-max justify-center"
-      >
+    <section id="skills" className="animated-gradient overflow-hidden">
+      <div className=" py-12 md:py-16 relative flex min-w-full w-max justify-center">
         <p className="text-2xl md:text-4xl lg:text-5xl font-bold ml-8 mr-12 md:ml-20 md:mr-40 text-white">
           CREATIVO
         </p>
