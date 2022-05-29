01d15a45916ccc380651caa5a2ac5317056d9dc5
Oscar España <espaaoscar@gmail.com>
2022-05-29 13:10:11 -0500

    update the content of portfolio section

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -9,28 +10,33 @@ const Portfolio = () => {
   const titleRef = useRef(null);
 
   useEffect(() => {
-    const tl2 = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#portafolio",
-        scrub: true,
-        pin: true,
-        start: "top top",
-        end: () => "250%",
-        anticipatePin: 1,
+    const tl2 = gsap.timeline(
+      {
+        scrollTrigger: {
+          trigger: "#portafolio",
+          scrub: true,
+          pin: true,
+          snap: 1,
+          start: "top top",
+          end: () => "+=1000",
+          anticipatePin: 1,
+          invalidateOnRefresh: true,
+        },
       },
-    });
+      { defaults: { ease: "power2.inOut" } }
+    );
     tl2.to(titleRef.current, {
-      transform: "scale(90)",
-      transformOrigin: "94.52% 57%",
+      transform: "scale(100)",
+      transformOrigin: "94.075% 57%",
       duration: 4,
     });
     tl2.to(
       backgroundRef.current,
       {
-        background: "transparent",
+        background: "black",
         duration: 1,
       },
-      "-=2.5"
+      "-=3"
     );
     tl2.to(
       backgroundRef.current,
@@ -38,51 +44,53 @@ const Portfolio = () => {
         autoAlpha: 0,
         duration: 1,
       },
-      "-=2.5"
+      "-=2"
     );
   }, []);
 
   return (
-    <section id="portafolio" className="relative">
-      <article
-        id="content"
-        className="absolute min-h-screen w-full top-0 bg-white flex items-center justify-center"
-      >
-        <div className="container">
-          <h2 className="text-primary">Portafolio</h2>
-          <p className="text-black">
-            Amet ex commodo Lorem nulla Lorem ad.Id reprehenderit ea dolore
-            officia mollit proident qui labore ipsum dolore est labore. Sunt
-            velit sunt sunt nisi id eu commodo. Ex laboris eiusmod quis ex
-            reprehenderit cupidatat anim exercitation duis ea. Aliqua laborum
-            deserunt sunt aliquip nisi aute nostrud reprehenderit ex
-            reprehenderit Lorem ad culpa. Enim aute ullamco exercitation amet
-            quis incididunt consectetur ad eiusmod id nostrud. Nulla magna enim
-            nisi Lorem minim adipisicing occaecat dolore labore irure laboris.
-            Consequat aliquip quis nisi fugiat pariatur eiusmod aliquip. Laborum
-            aliquip Lorem magna dolore excepteur id pariatur est eiusmod sunt.
-            Adipisicing et aliquip magna ipsum ad sint in excepteur cupidatat
-            Lorem nostrud. Et ut ex irure fugiat cupidatat. Sit aute cupidatat
-            nisi minim reprehenderit nisi. Ad quis do elit do et in est do amet
-            occaecat nisi aliqua occaecat. In magna consequat magna ut laborum
-            consequat ullamco. Dolor eiusmod ex ullamco veniam cupidatat minim
-            ad aliqua. Cillum sunt cupidatat sit cillum exercitation mollit
-            consectetur cillum amet id. Quis qui aute laborum ex ex veniam
-            mollit. Fugiat eu excepteur adipisicing consectetur laboris aliquip.
-            Tempor minim est reprehenderit in id exercitation esse officia ex
-            Lorem velit adipisicing.
-          </p>
+    <>
+      <section id="portafolio" className="relative overflow-hidden bg-black">
+        <div
+          id="content"
+          className="relative min-h-screen w-full flex items-center justify-center container"
+        >
+          <div className="absolute bg-primary top-[-750px] left-[-750px] w-[1000px] h-[1000px] rounded-full shadow-[0px_0px_16px_76px_rgb(231_40_137_/_50%)] blur-[175px] opacity-[55%]" />
+          <div className="absolute bg-violet top-[40%] right-[-100px] w-[150px] h-[150px] rounded-full shadow-[0px_0px_16px_76px_rgb(231_40_137_/_50%)] blur-[125px] opacity-[55%]" />
+          <h2 className="text-white">Descubre mis proyectos</h2>
         </div>
-      </article>
-      <div
-        ref={backgroundRef}
-        className="relative h-screen bg-primary flex items-center justify-center overflow-hidden"
-      >
-        <h2 ref={titleRef} className="text-white">
-          Portafolio
-        </h2>
-      </div>
-    </section>
+        <div
+          ref={backgroundRef}
+          className="absolute top-0 min-h-screen w-full bg-primary flex items-center justify-center overflow-hidden transition-colors duration-300"
+        >
+          <h2 ref={titleRef} className="text-white">
+            Portafolio
+          </h2>
+        </div>
+      </section>
+      <section className="bg-black">
+        <article className="container grid grid-cols-1 md:grid-cols-12 gap-5 py-24">
+          <div className=" md:col-span-4">
+            <h3 className="text-white">Grupo Transoecánica</h3>
+            <p className="text-white mt-16">
+              Aplicación web destinada a la automatización de procesos que
+              llevan las operadoras de vuelo con respecto a la programación de
+              vuelos.
+            </p>
+          </div>
+          <div className="md:col-span-7 md:col-end-13 relative">
+            <Image
+              src="/img/2x/grupo-transpoint.png"
+              alt="Grupo Transpoint"
+              width={810}
+              height={392}
+              layout="responsive"
+            />
+            <div className="absolute w-full h-full bg-black top-0 opacity-[70%]" />
+          </div>
+        </article>
+      </section>
+    </>
   );
 };
 
