650873496ba1d8e93c98f784bc8ce225bf247411
Oscar España <espaaoscar@gmail.com>
2022-06-16 21:47:24 -0500

    update the portfolio design

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -9,76 +9,32 @@ const Portfolio = () => {
   const backgroundRef = useRef(null);
   const titleRef = useRef(null);
 
-  useEffect(() => {
-    const tl2 = gsap.timeline(
-      {
-        scrollTrigger: {
-          trigger: "#portafolio",
-          scrub: true,
-          pin: true,
-          snap: 1,
-          start: "top top",
-          end: () => "+=1000",
-          anticipatePin: 1,
-          invalidateOnRefresh: true,
-        },
-      },
-      { defaults: { ease: "power2.inOut" } }
-    );
-    tl2.to(titleRef.current, {
-      transform: "scale(100)",
-      transformOrigin: "94.075% 57%",
-      duration: 4,
-    });
-    tl2.to(
-      backgroundRef.current,
-      {
-        background: "black",
-        duration: 1,
-      },
-      "-=3"
-    );
-    tl2.to(
-      backgroundRef.current,
-      {
-        autoAlpha: 0,
-        duration: 1,
-      },
-      "-=2"
-    );
-  }, []);
-
   return (
     <>
-      <section id="portafolio" className="relative overflow-hidden bg-black">
-        <div
-          id="content"
-          className="relative min-h-screen w-full flex items-center justify-center container"
-        >
-          <div className="absolute bg-primary top-[-750px] left-[-750px] w-[1000px] h-[1000px] rounded-full shadow-[0px_0px_16px_76px_rgb(231_40_137_/_50%)] blur-[175px] opacity-[55%]" />
-          <div className="absolute bg-violet top-[40%] right-[-100px] w-[150px] h-[150px] rounded-full shadow-[0px_0px_16px_76px_rgb(231_40_137_/_50%)] blur-[125px] opacity-[55%]" />
-          <h2 className="text-white">Descubre mis proyectos</h2>
-        </div>
-        <div
-          ref={backgroundRef}
-          className="absolute top-0 min-h-screen w-full bg-primary flex items-center justify-center overflow-hidden transition-colors duration-300"
-        >
-          <h2 ref={titleRef} className="text-white">
+      <section
+        id="portafolio"
+        className="bg-black min-h-screen w-full flex items-center justify-center"
+      >
+        <div ref={backgroundRef} className="container text-center">
+          <h2 ref={titleRef} className="text-primary text-8xl">
             Portafolio
           </h2>
+          <p className="text-white mt-8 text-3xl">Oscar España</p>
         </div>
       </section>
-      <section className="bg-black">
+      <section className="bg-gray">
         <article className="container grid grid-cols-1 md:grid-cols-12 gap-5 py-24">
           <div className=" md:col-span-4">
-            <h3 className="text-white">Grupo Transoecánica</h3>
-            <p className="text-white mt-16">
+            <h3 className="text-black">
+              Grupo <span className="text-primary">Transoecánica</span>
+            </h3>
+            <p className="text-darkGray mt-12">
               Aplicación web destinada a la automatización de procesos que
               llevan las operadoras de vuelo con respecto a la programación de
               vuelos.
             </p>
           </div>
-          <div className="md:col-span-7 md:col-end-13 relative">
+          <div className="md:col-span-7 md:col-end-13 relative shadow-xl">
             <Image
               src="/img/2x/grupo-transpoint.png"
               alt="Grupo Transpoint"
@@ -86,7 +42,6 @@ const Portfolio = () => {
               height={392}
               layout="responsive"
             />
-            <div className="absolute w-full h-full bg-black top-0 opacity-[70%]" />
           </div>
         </article>
       </section>
