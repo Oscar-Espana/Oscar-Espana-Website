a30df36c282bb1a3e55f297ecd413b96ed4896aa
Oscar España <espaaoscar@gmail.com>
2022-06-17 23:27:28 -0500

    add a new item of portfolio, add animation

--- components/Portfolio/index.jsx
+++ components/Portfolio/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
@@ -6,46 +6,80 @@ import Image from "next/image";
 gsap.registerPlugin(ScrollTrigger);
 
 const Portfolio = () => {
-  const backgroundRef = useRef(null);
-  const titleRef = useRef(null);
+  useEffect(() => {
+    const paneles = gsap.utils.toArray(".panel");
+    paneles.forEach((panel, i) => {
+      if (i < paneles.length - 1) {
+        ScrollTrigger.create({
+          trigger: panel,
+          start: "top top",
+          pin: true,
+          pinSpacing: false,
+        });
+      }
+    });
+  }, []);
 
   return (
-    <>
-      <section
-        id="portafolio"
-        className="bg-black min-h-screen w-full flex items-center justify-center"
-      >
-        <div ref={backgroundRef} className="container text-center">
-          <h2 ref={titleRef} className="text-primary text-8xl">
+    <section>
+      <article className="bg-black panel">
+        <div className="container w-full h-full flex flex-col  justify-center items-center">
+          <h2 className="text-primary text-6xl md:text-7xl lg:text-8xl">
             Portafolio
           </h2>
           <p className="text-white mt-8 text-3xl">Oscar España</p>
         </div>
-      </section>
-      <section className="bg-gray">
-        <article className="container grid grid-cols-1 md:grid-cols-12 gap-5 py-24">
-          <div className=" md:col-span-4">
-            <h3 className="text-black">
-              Grupo <span className="text-primary">Transoecánica</span>
-            </h3>
-            <p className="text-darkGray mt-12">
-              Aplicación web destinada a la automatización de procesos que
-              llevan las operadoras de vuelo con respecto a la programación de
-              vuelos.
-            </p>
-          </div>
-          <div className="md:col-span-7 md:col-end-13 relative shadow-xl">
-            <Image
-              src="/img/2x/grupo-transpoint.png"
-              alt="Grupo Transpoint"
-              width={810}
-              height={392}
-              layout="responsive"
-            />
+      </article>
+      <div className="panel">
+        <article className="bg-gray z-10">
+          <div className="container grid grid-cols-1 md:grid-cols-12 gap-5  py-20 md:py-28 lg:py-36">
+            <div className=" md:col-span-4">
+              <h3 className="text-black">
+                Grupo <span className="text-primary">Transoecánica</span>
+              </h3>
+              <p className="text-darkGray mt-12">
+                Aplicación web destinada a la automatización de procesos que
+                llevan las operadoras de vuelo con respecto a la programación de
+                vuelos.
+              </p>
+            </div>
+            <div className="md:col-span-7 md:col-end-13 relative shadow-xl">
+              <Image
+                src="/img/2x/grupo-transpoint.png"
+                alt="Grupo Transpoint"
+                width={810}
+                height={392}
+                layout="responsive"
+              />
+            </div>
           </div>
         </article>
-      </section>
-    </>
+
+        <article className="bg-white">
+          <div className="container grid grid-cols-1 md:grid-cols-12 gap-5 py-20 md:py-28 lg:py-36">
+            <div className=" md:col-span-4">
+              <h3 className="text-black">
+                <span className="text-primary">Bella</span>
+              </h3>
+              <p className="text-darkGray mt-12">
+                Landing Page minimaslista y elegante que contiene una gran
+                variedad de animaciones que se activan por medio del scroll.
+                Realizada usando GSAP.
+              </p>
+            </div>
+            <div className="md:col-span-7 md:col-end-13 relative shadow-xl">
+              <Image
+                src="/img/bella.png"
+                alt="Bella"
+                width={810}
+                height={392}
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 };
 
