1cacfb0a8ee46792123df3b436ace3ed8e7e659b
Oscar Espana <espaaoscar@gmail.com>
2022-11-06 22:44:48 -0500

    feat: installing and setting up next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import { appWithTranslation } from "next-i18next";
+import { Raleway } from "@next/font/google";
 import "../styles/globals.css";
 
+const raleway = Raleway({
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  subsets: ["latin"],
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={raleway.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default appWithTranslation(MyApp);
--- pages/_document.js
+++ pages/_document.js
@@ -7,18 +7,7 @@ class MyDocument extends Document {
       this.props.__NEXT_DATA__.locale || i18nextConfig.i18n.defaultLocale;
     return (
       <Html lang={currentLocale} dir="ltr" className="scroll-smooth">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
